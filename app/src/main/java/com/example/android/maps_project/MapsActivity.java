3745bd850ac57dca5aa605a24abe0b1109d3a779
karlcode <karl.l1@hotmail.com>
2017-11-02 20:51:55 +1100

    setOptionsMenu on map fragment for visible title bar

--- app/src/main/java/com/example/android/maps_project/MapsActivity.java
+++ app/src/main/java/com/example/android/maps_project/MapsActivity.java
@@ -33,11 +33,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         setContentView(R.layout.activity_maps);
 
 
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         //MapFragment  != MapView (rectangular section on screen with map within)
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        mapFragment.setHasOptionsMenu(true);
         Log.i("onCreate", "Debugging message for onCreate");
 
 
